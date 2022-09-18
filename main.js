9515aa775833b28f90069c6f209d6c47e56ca99d
shokiami <kiami.sho@gmail.com>
2022-09-18 00:25:07 -0700

    Some code reformatting.

--- main.js
+++ main.js
@@ -1,36 +1,46 @@
-// On Window Load
+// Main
+
+onload = main;
 
 function main() {
-  const canvas = new Canvas();
-  function refresh() {
-    canvas.angle_x += 0.01;
-    canvas.angle_y += 0.02;
-    canvas.angle_z += 0.03;
-    // clear canvas
-    canvas.clear();
-    // front face
-    canvas.triangle( 1,  1,  1, -1,  1,  1, -1, -1,  1,  0,  0,  1,  1);
-    canvas.triangle( 1, -1,  1,  1,  1,  1, -1, -1,  1,  0,  0,  1,  1);
-    // back face
-    canvas.triangle(-1,  1, -1,  1,  1, -1, -1, -1, -1,  0,  0,  1,  1);
-    canvas.triangle( 1,  1, -1,  1, -1, -1, -1, -1, -1,  0,  0,  1,  1);
-    // top face
-    canvas.triangle( 1,  1, -1, -1,  1, -1, -1,  1,  1,  0,  0,  1,  1);
-    canvas.triangle( 1,  1,  1,  1,  1, -1, -1,  1,  1,  0,  0,  1,  1);
-    // bottom face
-    canvas.triangle(-1, -1, -1,  1, -1, -1, -1, -1,  1,  0,  0,  1,  1);
-    canvas.triangle( 1, -1, -1,  1, -1,  1, -1, -1,  1,  0,  0,  1,  1);
-    // right face
-    canvas.triangle( 1,  1, -1,  1,  1,  1,  1, -1,  1,  0,  0,  1,  1);
-    canvas.triangle( 1, -1, -1,  1,  1, -1,  1, -1,  1,  0,  0,  1,  1);
-    // left face
-    canvas.triangle(-1,  1,  1, -1,  1, -1, -1, -1,  1,  0,  0,  1,  1);
-    canvas.triangle(-1,  1, -1, -1, -1, -1, -1, -1,  1,  0,  0,  1,  1);
-    // render canvas
-    canvas.render();
-    requestAnimationFrame(refresh);
+  init();
+  function loop() {
+    update();
+    requestAnimationFrame(loop);
   }
-  requestAnimationFrame(refresh);
+  requestAnimationFrame(loop);
 }
 
-window.onload = main;
+var canvas = null;
+
+function init() {
+  canvas = new Canvas();
+}
+
+function update() {
+  canvas.angle_x += 0.01;
+  canvas.angle_y += 0.02;
+  canvas.angle_z += 0.03;
+  // clear canvas
+  canvas.clear();
+  // front face
+  canvas.triangle( 1,  1,  1, -1,  1,  1, -1, -1,  1,  0,  0,  1,  1);
+  canvas.triangle( 1, -1,  1,  1,  1,  1, -1, -1,  1,  0,  0,  1,  1);
+  // back face
+  canvas.triangle(-1,  1, -1,  1,  1, -1, -1, -1, -1,  0,  0,  1,  1);
+  canvas.triangle( 1,  1, -1,  1, -1, -1, -1, -1, -1,  0,  0,  1,  1);
+  // top face
+  canvas.triangle( 1,  1, -1, -1,  1, -1, -1,  1,  1,  0,  0,  1,  1);
+  canvas.triangle( 1,  1,  1,  1,  1, -1, -1,  1,  1,  0,  0,  1,  1);
+  // bottom face
+  canvas.triangle(-1, -1, -1,  1, -1, -1, -1, -1,  1,  0,  0,  1,  1);
+  canvas.triangle( 1, -1, -1,  1, -1,  1, -1, -1,  1,  0,  0,  1,  1);
+  // right face
+  canvas.triangle( 1,  1, -1,  1,  1,  1,  1, -1,  1,  0,  0,  1,  1);
+  canvas.triangle( 1, -1, -1,  1,  1, -1,  1, -1,  1,  0,  0,  1,  1);
+  // left face
+  canvas.triangle(-1,  1,  1, -1,  1, -1, -1, -1,  1,  0,  0,  1,  1);
+  canvas.triangle(-1,  1, -1, -1, -1, -1, -1, -1,  1,  0,  0,  1,  1);
+  // render canvas
+  canvas.render();
+}
