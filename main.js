3791d151007e821faa9a7e18fe6ae2f9280fdd9a
shokiami <kiami.sho@gmail.com>
2022-10-12 13:32:46 -0700

    better ripples

--- main.js
+++ main.js
@@ -66,8 +66,13 @@ function draw_ripples() {
     let r = ripple.r;
     let res = 1000;
     let dtheta = 2 * Math.PI / res;
-    for (let i = 0; i <= res; i++) {
-      canvas.triangle(x + r * Math.cos(i * dtheta), y + r * Math.sin(i * dtheta), 0, x + r * Math.cos(i * dtheta) + 0.01, y + r * Math.sin(i * dtheta), 0, x + r * Math.cos(i * dtheta), y + r * Math.sin(i * dtheta) + 0.01, 0, 1, 1, 1, 1);
+    for (let i = 0; i < res; i++) {
+      let cos1 = Math.cos(i * dtheta);
+      let sin1 = Math.sin(i * dtheta);
+      let cos2 = Math.cos((i + 1) * dtheta);
+      let sin2 = Math.sin((i + 1) * dtheta);
+      canvas.triangle(x + r * cos1, y + r * sin1, 0, x + r * cos2, y + r * sin2, 0, x + (r - 0.01) * cos1, y + (r - 0.01) * sin1, 0, 1, 1, 1, 1);
+      canvas.triangle(x + (r - 0.01) * cos1, y + (r - 0.01) * sin1, 0,  x + r * cos2, y + r * sin2, 0, x + (r - 0.01) * cos2, y + (r - 0.01) * sin2, 0, 1, 1, 1, 1);
     }
   }
 }
