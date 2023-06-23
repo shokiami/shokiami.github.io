ea6e3c26ae4631eac2477b9aaf390b6baf591857
shokiami <kiami.sho@gmail.com>
2023-06-23 04:12:04 -0700

    better coloring

--- mandelbrot.cc
+++ mandelbrot.cc
@@ -11,14 +11,15 @@ using std::vector;
 using std::min;
 using std::max;
 
-#define WIDTH 1920
-#define HEIGHT 1080
+#define WIDTH 3840
+#define HEIGHT 2160
 #define MAX_ITR 1000
 #define BAILOUT 256
+#define GRADIENT 0.35
 
 const vector<png::rgb_pixel> palette = {
-  {66, 30, 15}, {25, 7, 26}, {9, 1, 47}, {4, 4, 73}, {0, 7, 100}, {1, 44, 138}, {24, 82, 177}, {57, 125, 209},
-  {134, 181, 229}, {211, 236, 248}, {241, 233, 191}, {248, 201, 95}, {255, 170, 0}, {204, 128, 0}, {153, 87, 0}, {106, 52, 3}
+  {9, 1, 47}, {4, 4, 73}, {0, 7, 100}, {1, 44, 138}, {24, 82, 177}, {57, 125, 209}, {134, 181, 229}, {211, 236, 248},
+  {241, 233, 191}, {248, 201, 95}, {255, 170, 0}, {204, 128, 0}, {153, 87, 0}, {106, 52, 3}, {66, 30, 15}, {25, 7, 26}
 };
 
 void render(double real, double imag, double zoom, string filename) {
@@ -38,9 +39,11 @@ void render(double real, double imag, double zoom, string filename) {
         z_r2 = z_r * z_r;
         z_i2 = z_i * z_i;
         if (z_r2 + z_i2 > BAILOUT * BAILOUT) {
-          double p = min(max(1.0 - log2(0.5 * log(z_r2 + z_i2) / log(BAILOUT)), 0.0), 1.0);
-          png::rgb_pixel color_1 = palette[itr % palette.size()];
-          png::rgb_pixel color_2 = palette[(itr + 1) % palette.size()];
+          double itr_s = GRADIENT * (itr + min(max(1.0 - log2(0.5 * log(z_r2 + z_i2) / log(BAILOUT)), 0.0), 1.0));
+          int itr_f = itr_s;
+          double p = itr_s - itr_f;
+          png::rgb_pixel color_1 = palette[itr_f % palette.size()];
+          png::rgb_pixel color_2 = palette[(itr_f + 1) % palette.size()];
           int r = (1 - p) * color_1.red + p * color_2.red;
           int g = (1 - p) * color_1.green + p * color_2.green;
           int b = (1 - p) * color_1.blue + p * color_2.blue;
@@ -54,7 +57,7 @@ void render(double real, double imag, double zoom, string filename) {
 }
 
 int main() {
-  render(0.0, 0.0, 1.0, "mandelbrot_set.png");
+  render(-0.6, 0.0, 1.5, "mandelbrot_set.png");
   render(-0.8, 0.2, 10.0, "seahorse_valley.png");
   return EXIT_SUCCESS;
 }
