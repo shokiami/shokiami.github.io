bdc22871006d85917d1818f8344e19c58d6728b7
shokiami <kiami.sho@gmail.com>
2023-06-28 14:45:54 -0700

    updated target positions

--- main.js
+++ main.js
@@ -25,36 +25,38 @@ function init() {
 }
 
 function update() {
-  let scroll = NUM_IMAGES * Math.min(Math.max(window.scrollY / (document.documentElement.scrollHeight - window.innerHeight), 0.0), 1.0);
+  let scroll = Math.min(Math.max(window.scrollY / (document.documentElement.scrollHeight - window.innerHeight), 0.0), 1.0);
+  let scroll_offset = 0.07;
+  let i_cont = (1.0 - scroll) * scroll_offset + scroll * NUM_IMAGES;
   for (image of document.querySelectorAll('.image')) {
     // update visibility
     let i = parseInt(image.id);
-    if (scroll < i || scroll > i + 2) {
+    if (i_cont < i || i_cont > i + 2) {
       image.style.setProperty('visibility', 'hidden');
       continue;
     }
     image.style.setProperty('visibility', 'visible');
     // update scale
-    let scale = 1.02 * Math.min(2.0 ** (scroll - i), 4.0);
+    let scale = 1.02 * Math.min(2.0 ** (i_cont - i), 4.0);
     image.style.setProperty('transform', 'translate(-50%, -50%) scale(' + scale + ')');
     // update opacity
     let opacity = 1.0;
     if (i > 0) {
-      opacity = Math.min(scroll - i, 1.0);
+      opacity = Math.min(i_cont - i, 1.0);
     }
     image.style.setProperty('opacity', opacity);
     // update size
     let width = Math.max(window.innerHeight / window.innerWidth * image.width / image.height, 1.0);
     image.style.setProperty('width', 100 * width + '%');
     // update position
-    let target = 0.67;
-    let left = target - (target - 0.5) * Math.pow(2.0, -0.5 * scroll);
+    let target = 0.69;
+    let left = target + (0.46 - target) * Math.pow(2.0, -0.5 * i_cont);
     image.style.setProperty('left', 100 * left + '%');
-    let top = 0.5 - (0.53 - 0.5) * Math.pow(2.0, -0.5 * scroll);
+    let top = 0.5 + (0.47 - 0.5) * Math.pow(2.0, -0.5 * i_cont);
     image.style.setProperty('top', 100 * top + '%');
   }
   // update fog
-  let left = -Math.pow(2.0, -1.0 * scroll);
+  let left = -Math.pow(2.0, -1.0 * i_cont);
   console.log(left);
   let fog = document.getElementById('fog');
   fog.style.setProperty('left', 100 * left + '%');
