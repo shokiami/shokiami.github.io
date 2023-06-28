f20d0e7df1948fc9c3a0f02c96c7149005f68441
shokiami <kiami.sho@gmail.com>
2023-06-27 21:40:14 -0700

    clean up

--- main.js
+++ main.js
@@ -1,12 +1,10 @@
-const WIDTH = 3840;
-const HEIGHT = 2160;
-const NUM_IMGS = 51;
+const NUM_IMAGES = 51;
 
 window.onload = init;
 
 function init() {
   let image_container = document.getElementById('image-container');
-  for (let i = 0; i < NUM_IMGS; i++) {
+  for (let i = 0; i < NUM_IMAGES; i++) {
     let image = document.createElement('img');
     image.id = i;
     image.src = 'images/' + i + '.png';
@@ -17,7 +15,7 @@ function init() {
 }
 
 function update() {
-  let curr = NUM_IMGS * Math.min(window.scrollY / (document.documentElement.scrollHeight - window.innerHeight), 0.999);
+  let curr = NUM_IMAGES * Math.min(window.scrollY / (document.documentElement.scrollHeight - window.innerHeight), 0.999);
   for (image of document.querySelectorAll('.image')) {
     let i = parseInt(image.id);
     if (curr < i || curr > i + 1) {
@@ -27,7 +25,7 @@ function update() {
     image.style.setProperty('visibility', 'visible');
     let scale = Math.min(2.0 ** (curr - i), 2.0);
     image.style.setProperty('transform', 'scale(' + scale + ')');
-    let width = Math.max(window.innerHeight / window.innerWidth * WIDTH / HEIGHT, 1.0);
+    let width = Math.max(window.innerHeight / window.innerWidth * image.width / image.height, 1.0);
     image.style.setProperty('width', 100 * width + '%');
   }
   window.requestAnimationFrame(update);
