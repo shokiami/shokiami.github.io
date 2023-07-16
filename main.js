6f856ff69a2a860577994b28ff47310e0022e101
shokiami <kiami.sho@gmail.com>
2023-07-15 18:33:03 -0700

    vertical navbar

--- main.js
+++ main.js
@@ -1,16 +1,18 @@
-const NUM_IMAGES = 140;
-const SCALAR = 1.28402541669;  // e^(1/4)
-const IMAGES_DIR = 'images/mandelbrot/';
+const IMAGE_COUNT = 140;
+const IMAGE_HEIGHT = 1080;
+const IMAGE_WIDTH = 1920;
+const IMAGE_SCALAR = 1.28402541669;  // e^(1/4)
+const IMAGE_DIR = 'images/mandelbrot/';
 
 window.onload = init;
 
 function init() {
   // init images
   let image_container = document.getElementById('image-container');
-  for (let i = 0; i < NUM_IMAGES; i++) {
+  for (let i = 0; i < IMAGE_COUNT; i++) {
     let image = document.createElement('img');
     image.id = i;
-    image.src = IMAGES_DIR + i + '.jpg';
+    image.src = IMAGE_DIR + i + '.jpg';
     image.className = 'image';
     image_container.appendChild(image);
   }
@@ -19,7 +21,7 @@ function init() {
 
 function update() {
   let scroll = Math.min(Math.max(window.scrollY / (document.documentElement.scrollHeight - window.innerHeight), 0.0), 1.0);
-  let i_cont = scroll * (NUM_IMAGES - 1);
+  let i_cont = scroll * (IMAGE_COUNT - 1);
   for (image of document.querySelectorAll('.image')) {
     // update visibility
     let i = parseInt(image.id);
@@ -29,19 +31,19 @@ function update() {
     }
     image.style.setProperty('visibility', 'visible');
     // update size
-    let height = 100.0 * Math.max(image.height / image.width * window.innerWidth / window.innerHeight, 1.0) + '%';
+    let height = 100.0 * Math.max(IMAGE_HEIGHT / IMAGE_WIDTH * window.innerWidth / window.innerHeight, 1.0) + '%';
     image.style.setProperty('height', height);
     // update scale
-    let scale = SCALAR ** (i_cont - i);
-    let dx = 50.0 - 100.0 * (0.9 - 0.25 * 0.9 ** i) + '%';
+    let scale = IMAGE_SCALAR ** (i_cont - i);
     let dy = 50.0 - 100.0 * (0.2 + 0.14 * 0.9 ** i) + '%';
+    let dx = 50.0 - 100.0 * (0.9 - 0.25 * 0.9 ** i) + '%';
     let transform = 'translate(-50%, -50%) scale(' + scale + ') translate(' + dx + ', ' + dy + ')';
     image.style.setProperty('transform', transform);
     // update position
+    let top = 100.0 * (0.2 + 0.14 * 0.9 ** i_cont) + '%';
     let left = 100.0 * (0.9 - 0.25 * 0.9 ** i_cont) + '%';
-    let top = 100.0 * (0.2 + 0.18 * 0.9 ** i_cont) + '%';
-    image.style.setProperty('left', left);
     image.style.setProperty('top', top);
+    image.style.setProperty('left', left);
     // update opacity
     let opacity = 1.0;
     if (i > 0) {
